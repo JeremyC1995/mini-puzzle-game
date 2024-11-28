abc9eabef89453924ff526722401e7860b40584a
JeremyC1995 <cssj1995@gmail.com>
2024-11-28 16:56:26 +0800

    add keypress event and click event

--- src/ui/GameJFrame.java
+++ src/ui/GameJFrame.java
@@ -1,11 +1,22 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class GameJFrame extends JFrame {
+public class GameJFrame extends JFrame implements KeyListener {
+    private ArrayList<Integer> data;
+    private int index;
+    private int pictureIndex;
+    private final ArrayList<Integer> winData =
+            new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0));
+    private int step = 0;
+
     public GameJFrame() {
         super("Puzzle Game");
         initJFrame();
@@ -14,14 +25,43 @@ public class GameJFrame extends JFrame {
         setVisible(true);
     }
 
-    private void initImage() {
-        List<Integer> indices = randomIndices();
+    private boolean victory() {
         for (int i = 0; i < 16; i++) {
-            ImageIcon image = new ImageIcon("image\\animal\\animal3\\" + indices.get(i) + ".jpg");
+            if (!this.data.get(i).equals(this.winData.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initImage() {
+        //print data in 4x4 format
+        for (int i = 0; i < 16; i++) {
+            System.out.print(this.data.get(i) + " ");
+            if (i % 4 == 3) {
+                System.out.println();
+            }
+        }
+        this.getContentPane().removeAll();
+
+        for (int i = 0; i < 16; i++) {
+            ImageIcon image = new ImageIcon("image\\animal\\animal" + this.pictureIndex + "\\" + this.data.get(i) + ".jpg");
             JLabel label = new JLabel(image);
-            label.setBounds(105 * (i % 4)+83, 105 * (i / 4)+134, 105, 105);
+            label.setBounds(105 * (i % 4) + 83, 105 * (i / 4) + 134, 105, 105);
+            label.setBorder(new BevelBorder(BevelBorder.LOWERED));
             this.getContentPane().add(label);
         }
+
+        //Step count
+        JLabel stepCount = new JLabel("Step: " + this.step);
+        stepCount.setBounds(50, 30, 100, 30);
+        this.getContentPane().add(stepCount);
+
+        this.getContentPane().repaint();
+        if (this.victory()) {
+            JOptionPane.showMessageDialog(this, "You Win!", "Victory", JOptionPane.INFORMATION_MESSAGE);
+        }
+
     }
 
     private void initJMenuBar() {
@@ -45,6 +85,13 @@ public class GameJFrame extends JFrame {
         fileMenu.add(exitItem);
         aboutUs.add(contactUsItem);
 
+        //add ActionListener
+        newGameItem.addActionListener(e -> {
+            System.out.println("New Game");
+            this.randomIndices();
+            this.step = 0;
+            this.initImage();
+        });
         // Add menus to the menu bar
         menuBar.add(fileMenu);
         menuBar.add(aboutUs);
@@ -60,14 +107,95 @@ public class GameJFrame extends JFrame {
         setLocationRelativeTo(null);
 
         setLayout(null);
+        randomIndices();
+        addKeyListener(this);
     }
 
-    private List<Integer> randomIndices() {
-        List<Integer> indices = new ArrayList<>();
+    private void randomIndices() {
+        ArrayList<Integer> indices = new ArrayList<>();
         for (int i = 0; i < 16; i++) {
             indices.add(i);
         }
         Collections.shuffle(indices);
-        return indices;
+        this.index = indices.indexOf(0);
+        this.pictureIndex = new Random().nextInt(5) + 1;
+        this.data = indices;
     }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_A) {
+            this.getContentPane().removeAll();
+            ImageIcon image = new ImageIcon("image\\animal\\animal" + this.pictureIndex + "\\all.jpg");
+            JLabel all = new JLabel(image);
+            all.setBounds(83, 134, 420, 420);
+            this.getContentPane().add(all);
+            this.getContentPane().repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //up left down right
+        if (this.victory()) {
+            return;
+        }
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_UP:
+                if (this.index > 3) {
+                    Collections.swap(this.data, this.index, this.index - 4);
+                    this.index -= 4;
+                    step++;
+                    initImage();
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if (this.index < 12) {
+                    Collections.swap(this.data, this.index, this.index + 4);
+                    this.index += 4;
+                    step++;
+                    initImage();
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (this.index % 4 != 0) {
+                    Collections.swap(this.data, this.index, this.index - 1);
+                    this.index -= 1;
+                    step++;
+                    initImage();
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if (this.index % 4 != 3) {
+                    Collections.swap(this.data, this.index, this.index + 1);
+                    this.index += 1;
+                    step++;
+                    initImage();
+                }
+                break;
+            case KeyEvent.VK_A:
+                initImage();
+                break;
+            case KeyEvent.VK_W:
+                //order the data
+                this.data.remove(this.index);
+                Collections.sort(this.data);
+                this.data.add(0);
+                this.index = 15;
+                initImage();
+                break;
+            default:
+                System.out.println("Other key released");
+                break;
+        }
+
+    }
+
 }
